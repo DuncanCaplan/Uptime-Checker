7517f540a0ebaf5e9ea0689e5eb6c4dbb8ffa4e0
DuncanCaplan <duncan.caplan@outlook.com>
2026-02-02 18:21:26 +0000

    Added GET method with error handling

--- checker.py
+++ checker.py
@@ -1,7 +1,16 @@
 import requests
 
-urls = open("urls.txt")
+urls = open("urls.txt").readlines()
 
-print(urls.read())
+for line in urls:
+    url = line.strip()
+    try:
+        response = requests.request("GET", url)
 
-urls.close()
+        if response.status_code != 200:
+            print(f"URL {url} is not reachable. Status code: {response.status_code}")
+        else:
+            print(f"URL {url} is reachable.")
+    except requests.exceptions.RequestException as e:
+        print(f"URL {url} is not reachable. Error: {e}")                   
+
