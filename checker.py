8e6a250ed0228af6240e78e7be11de409caad18c
Duncan Caplan <duncan.caplan@outlook.com>
2026-02-03 19:16:25 +0000

    Paeallel execution

--- checker.py
+++ checker.py
@@ -4,21 +4,31 @@ from colorama import Fore, init
 
 init(autoreset=True)
 
-with open(sys.argv[1], 'r') as urls_file:
-    
+
+def check_url(url):
+    url = url.strip()
+
+    try:
+        response = requests.request("GET", url)
+
+        if response.status_code != 200:
+            print(
+                Fore.RED
+                + f"URL {url} is not reachable. Status code: {response.status_code}"
+            )
+        else:
+            print(Fore.GREEN + f"URL {url} is reachable.")
+            print(
+                Fore.CYAN
+                + f"Response time: {response.elapsed.total_seconds()} seconds."
+            )
+    except requests.exceptions.RequestException as e:
+        print(Fore.YELLOW + f"URL {url} is not reachable. Error: {e}")
+
+
+with open(sys.argv[1], "r") as urls_file:
 
     urls = urls_file.readlines()
 
-    for line in urls:
-        url = line.strip()
-        try:
-            response = requests.request("GET", url)
-
-            if response.status_code != 200:
-                print(Fore.RED + f"URL {url} is not reachable. Status code: {response.status_code}")
-            else:
-                print(Fore.GREEN + f"URL {url} is reachable.")
-                print(Fore.CYAN + f"Response time: {response.elapsed.total_seconds()} seconds.")
-        except requests.exceptions.RequestException as e:
-            print(Fore.YELLOW + f"URL {url} is not reachable. Error: {e}")                   
-
+with concurrent.futures.ThreadPoolExecutor() as executor:
+    executor.map(check_url, urls)
