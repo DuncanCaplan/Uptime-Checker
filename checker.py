b7264ed6ef31e69865dcd1fa78643f4f026ff555
Duncan Caplan <duncan.caplan@outlook.com>
2026-02-03 18:55:57 +0000

    Enclosed function in 'with' block

--- checker.py
+++ checker.py
@@ -1,21 +1,24 @@
-import requests, colorama, sys
+import requests, colorama, sys, concurrent.futures
 
 from colorama import Fore, init
 
 init(autoreset=True)
 
-urls = open(sys.argv[1]).readlines()
+with open(sys.argv[1], 'r') as urls_file:
+    
 
-for line in urls:
-    url = line.strip()
-    try:
-        response = requests.request("GET", url)
+    urls = urls_file.readlines()
 
-        if response.status_code != 200:
-            print(Fore.RED + f"URL {url} is not reachable. Status code: {response.status_code}")
-        else:
-            print(Fore.GREEN + f"URL {url} is reachable.")
-            print(Fore.CYAN + f"Response time: {response.elapsed.total_seconds()} seconds.")
-    except requests.exceptions.RequestException as e:
-        print(Fore.YELLOW + f"URL {url} is not reachable. Error: {e}")                   
+    for line in urls:
+        url = line.strip()
+        try:
+            response = requests.request("GET", url)
+
+            if response.status_code != 200:
+                print(Fore.RED + f"URL {url} is not reachable. Status code: {response.status_code}")
+            else:
+                print(Fore.GREEN + f"URL {url} is reachable.")
+                print(Fore.CYAN + f"Response time: {response.elapsed.total_seconds()} seconds.")
+        except requests.exceptions.RequestException as e:
+            print(Fore.YELLOW + f"URL {url} is not reachable. Error: {e}")                   
 
