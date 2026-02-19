995c4cea150f8da52ecb51f2fd133a9180236dbe
Duncan Caplan <duncan.caplan@outlook.com>
2026-02-19 15:34:58 +0000

    Grouped error codes into ranges

--- checker.py
+++ checker.py
@@ -11,13 +11,35 @@ def check_url(url):
     try:
         response = requests.request("GET", url)
 
-        if response.status_code != 200:
+        if 200 <= response.status_code < 300:
+            print(
+                Fore.GREEN
+                + f"URL {url} is reachable. Status code: {response.status_code}"
+            )
+            print(
+                Fore.CYAN
+                + f"Response time: {response.elapsed.total_seconds()} seconds."
+            )
+        elif 300 <= response.status_code < 400:
+            print(Fore.YELLOW + f"URL {url} has been redirected.")
+            print(
+                Fore.CYAN
+                + f"Response time: {response.elapsed.total_seconds()} seconds."
+            )
+        elif 400 <= response.status_code < 500:
             print(
                 Fore.RED
-                + f"URL {url} is not reachable. Status code: {response.status_code}"
+                + f"URL {url} returned a client error status code: {response.status_code}"
             )
-        else:
-            print(Fore.GREEN + f"URL {url} is reachable.")
+            print(
+                Fore.CYAN
+                + f"Response time: {response.elapsed.total_seconds()} seconds."
+            )
+        elif 500 <= response.status_code < 600:
+            print(
+                Fore.RED
+                + f"URL {url} returned a server error status code: {response.status_code}"
+            )
             print(
                 Fore.CYAN
                 + f"Response time: {response.elapsed.total_seconds()} seconds."
