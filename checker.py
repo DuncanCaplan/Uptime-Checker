d4310135a3f7b584f1923c540b379113f9da8c8b
Duncan Caplan <duncan.caplan@outlook.com>
2026-02-24 12:13:12 +0000

    Added threading for server & url checker

--- checker.py
+++ checker.py
@@ -1,6 +1,4 @@
 import requests
-import sys
-import concurrent.futures
 import threading
 import time
 
@@ -22,109 +20,57 @@ def health():
 
 def check_all_urls():
     file = open("urls.txt", "r")
-
     urls = file.readlines()
-
-    print(urls)
-
-    for url in urls:
-        url = url.strip()
-
-        try:
-            response = requests.request("GET", url)
-
-            if 200 <= response.status_code < 300:
-                print(
-                    Fore.GREEN
-                    + f"URL {url} is reachable. Status code: {response.status_code}"
-                )
-                print(
-                    Fore.CYAN
-                    + f"Response time: {response.elapsed.total_seconds()} seconds."
-                )
-            elif 300 <= response.status_code < 400:
-                print(Fore.YELLOW + f"URL {url} has been redirected.")
-                print(
-                    Fore.CYAN
-                    + f"Response time: {response.elapsed.total_seconds()} seconds."
-                )
-            elif 400 <= response.status_code < 500:
-                print(
-                    Fore.RED
-                    + f"URL {url} returned a client error status code: {response.status_code}"
-                )
-                print(
-                    Fore.CYAN
-                    + f"Response time: {response.elapsed.total_seconds()} seconds."
-                )
-            elif 500 <= response.status_code < 600:
-                print(
-                    Fore.RED
-                    + f"URL {url} returned a server error status code: {response.status_code}"
-                )
-                print(
-                    Fore.CYAN
-                    + f"Response time: {response.elapsed.total_seconds()} seconds."
-                )
-        except requests.exceptions.RequestException as e:
-            print(Fore.YELLOW + f"URL {url} is not reachable. Error: {e}")
-
     file.close()
 
-'''
-@app.route("/")
-def uptime_checker():
-           
-    def check_url(url):
-        url = url.strip()
+    while True:
 
-        try:
-            response = requests.request("GET", url)
+        for url in urls:
+            url = url.strip()
 
-            if 200 <= response.status_code < 300:
-                print(
-                    Fore.GREEN
-                    + f"URL {url} is reachable. Status code: {response.status_code}"
-                )
-                print(
-                    Fore.CYAN
-                    + f"Response time: {response.elapsed.total_seconds()} seconds."
-                )
-            elif 300 <= response.status_code < 400:
-                print(Fore.YELLOW + f"URL {url} has been redirected.")
-                print(
-                    Fore.CYAN
-                    + f"Response time: {response.elapsed.total_seconds()} seconds."
-                )
-            elif 400 <= response.status_code < 500:
-                print(
-                    Fore.RED
-                    + f"URL {url} returned a client error status code: {response.status_code}"
-                )
-                print(
-                    Fore.CYAN
-                    + f"Response time: {response.elapsed.total_seconds()} seconds."
-                )
-            elif 500 <= response.status_code < 600:
-                print(
-                    Fore.RED
-                    + f"URL {url} returned a server error status code: {response.status_code}"
-                )
-                print(
-                    Fore.CYAN
-                    + f"Response time: {response.elapsed.total_seconds()} seconds."
-                )
-        except requests.exceptions.RequestException as e:
-            print(Fore.YELLOW + f"URL {url} is not reachable. Error: {e}")
+            try:
+                response = requests.request("GET", url)
+
+                if 200 <= response.status_code < 300:
+                    print(
+                        Fore.GREEN
+                        + f"URL {url} is reachable. Status code: {response.status_code}"
+                    )
+                    print(
+                        Fore.CYAN
+                        + f"Response time: {response.elapsed.total_seconds()} seconds."
+                    )
+                elif 300 <= response.status_code < 400:
+                    print(Fore.YELLOW + f"URL {url} has been redirected.")
+                    print(
+                        Fore.CYAN
+                        + f"Response time: {response.elapsed.total_seconds()} seconds."
+                    )
+                elif 400 <= response.status_code < 500:
+                    print(
+                        Fore.RED
+                        + f"URL {url} returned a client error status code: {response.status_code}"
+                    )
+                    print(
+                        Fore.CYAN
+                        + f"Response time: {response.elapsed.total_seconds()} seconds."
+                    )
+                elif 500 <= response.status_code < 600:
+                    print(
+                        Fore.RED
+                        + f"URL {url} returned a server error status code: {response.status_code}"
+                    )
+                    print(
+                        Fore.CYAN
+                        + f"Response time: {response.elapsed.total_seconds()} seconds."
+                    )
+            except requests.exceptions.RequestException as e:
+                print(Fore.YELLOW + f"URL {url} is not reachable. Error: {e}")
+
+        time.sleep(30)
 
 
-    with open(sys.argv[1], "r") as urls_file:
-        urls = urls_file.readlines()
-
-    with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
-        executor.map(check_url, urls)
-'''
-check_all_urls()
+threading.Thread(target = check_all_urls).start()
 
 if __name__ == "__main__":
     app.run(debug=True)
